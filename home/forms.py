68427c7852d9f63feadb22ca2d6bff68bf1eea24
BhargavSatya <bhargav.satya16@gmail.com>
2018-10-24 01:59:00 +0530

    add user management pages|learn etc

--- home/forms.py
+++ home/forms.py
@@ -13,3 +13,21 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name' , 'email', 'password1', 'password2')
+class ForgotpasswordForm(forms.Form):
+	username=forms.CharField(max_length=100)
+	email=forms.EmailField(max_length=100)
+
+	def clean(self):
+		cleaned_data=super(ForgotpasswordForm,self).clean()
+		Username=cleaned_data.get('Username')
+        # email=cleaned_data.get('email')
+class ProfileForm(forms.Form):
+    first_name=forms.CharField(max_length=200)
+    # last_name=forms.CharField(max_length=200)
+    email=forms.EmailField(max_length=200)
+
+    def clean(self):
+        cleaned_data =super(ProfileForm,self).clean()
+        first_name=cleaned_data.get('first_name')
+        # last_name=cleaned_data.get('last_name')
+        email=cleaned_data.get('email')		
--- home/views.py
+++ home/views.py
@@ -6,8 +6,13 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from .forms import *
+from django.contrib.auth.forms import *
+from forms import *
 from models import *
+from django.contrib import *
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
 
 # Create your views here.
 def index(request):
@@ -26,13 +31,81 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'auth/signup.html', {'form': form})
-
+def forgotpassword(request):
+    if request.method == 'POST':
+        form=ForgotpasswordForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            email=form.cleaned_data['email']
+            user_num=User.objects.filter(username=username,email=email).count()
+            
+            if user_num > 0 :
+                user=User.objects.get(username=username,email=email)
+                s=""
+                s="qwer123"
+                user.set_password('qwer123')
+                user.save()
+                return render(request,'forgotpassword/changepasswordsuccess.html',{'password':s})
+            else :
+                return render(request,'forgotpassword/changepaswordunsuccess.html')
+    else:
+        form=ForgotpasswordForm
+        return render(request,'forgotpassword/forgotpassword.html',{'form': form})
 
 
 def profile(request):
     #print request.user.id
     return render(request,'userdetails/profile.html')
 
+@login_required
+@transaction.atomic
+def update_profile(request):
+    if request.method == 'POST':
+        user=request.user
+        #user_form = UserForm(request.POST, instance=request.user)
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            first_name=form.cleaned_data['first_name']
+            # last_name=form.cleaned_data['last_name']
+            email=form.cleaned_data['email']
+            u=User.objects.get(username=user.username)
+            u.first_name=first_name
+            # u.last_name=last_name
+            u.email=email
+            u.save()
+            # string='You have sucesfully updated your profile with \n\nFirst name: '+first_name+'\nLast_name: '+last_name+'\nEmail: '+email             
+            # email1 = EmailMessage('SurveyTool', string, to=[email])
+            # email1.send()
+            #messages.success(request, _('Your profile was successfully updated!'))
+            return redirect('/profile')
+        else:
+            messages.error(request, _('Please correct the error below.'))
+    else:
+        #user_form = UserForm(instance=request.user)
+        # user=request.user
+        # u=User.objects.get(username=user.username)
+        profile_form = ProfileForm
+        # profile_form.first_name=u.first_name
+        # profile_last.first_name=u.last_name
+        # profile_form.first_name=u.first_name
+        return render(request, 'updateprofile/updateprofile.html', {'profile_form': profile_form})
+
+def changepassword(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            print "fff"
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('/profile')
+        else:
+            messages.error(request,('Please correct the error below.'))
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'changepassword/pass.html', {
+        'form': form
+}) 
 
 def sample_view(request):
     current_user = request.user
